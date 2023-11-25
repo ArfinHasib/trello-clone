d18a76e2e73a1f1658409c16c01cad2885f78f7f
ArfinHasib <arfinhasib21375@gmail.com>
2023-11-25 14:44:56 +0600

    Board Server Action

--- components/form/form-picker.tsx
+++ components/form/form-picker.tsx
@@ -3,10 +3,13 @@
 import { useEffect, useState } from 'react';
 
 import { unsplash } from '@/lib/unsplash';
-import { Loader2 } from 'lucide-react';
+import { Check, Loader2 } from 'lucide-react';
 import { useFormStatus } from 'react-dom';
 import { cn } from '@/lib/utils';
 import Image from 'next/image';
+import { defaultImages } from '@/constants/images';
+import Link from 'next/link';
+import { FormErrors } from './form-errors';
 
 interface FormPickerProps {
    id: string;
@@ -16,13 +19,16 @@ interface FormPickerProps {
 export const FormPicker = ({ id, errors }: FormPickerProps) => {
    const { pending } = useFormStatus();
 
-   const [imgaes, setImgaes] = useState<Array<Record<string, any>>>([]);
+   const [imgaes, setImgaes] = useState<Array<Record<string, any>>>([
+      defaultImages,
+   ]);
    const [isloading, setIsloading] = useState(true);
    const [selectedImageId, setSelectedImageId] = useState(null);
 
    useEffect(() => {
       const fetchImages = async () => {
          try {
+            // throw new Error('Unsplash Error');
             const result = await unsplash.photos.getRandom({
                collectionIds: ['317099'],
                count: 9,
@@ -36,7 +42,7 @@ export const FormPicker = ({ id, errors }: FormPickerProps) => {
             }
          } catch (error) {
             console.log(error);
-            setImgaes([]);
+            setImgaes(defaultImages);
          } finally {
             setIsloading(false);
          }
@@ -68,15 +74,38 @@ export const FormPicker = ({ id, errors }: FormPickerProps) => {
                      setSelectedImageId(image.id);
                   }}
                >
+                  <input
+                     type='radio'
+                     id={id}
+                     name={id}
+                     className='hidden'
+                     checked={selectedImageId === image.id}
+                     disabled={pending}
+                     value={`${image.id}|${image.urls.thumb}|${image.urls.full}|${image.links.html}|${image.user.name}`}
+                  />
                   <Image
                      fill
                      src={image.urls.thumb}
                      alt='unsplash image'
                      className='object-cover rounded-sm'
                   />
+                  {selectedImageId === image.id && (
+                     <div className='absolute inset-y-0 h-full w-full bg-black/30 flex items-center justify-center'>
+                        <Check className='h-4 w-4 text-white' />
+                     </div>
+                  )}
+                  <Link
+                     href={image.links.html}
+                     target='_blank'
+                     className='opacity-0 group-hover:opacity-100 absolute bottom-0 w-full text-[10px] truncate text-white hover:underline p-1 bg-black/50'
+                  >
+                     {image.user.name}
+                  </Link>
                </div>
             ))}
          </div>
+
+         <FormErrors id='image' errors={errors} />
       </div>
    );
 };
