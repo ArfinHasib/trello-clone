fa306319ef4bea5a653c5bffff6a53fb3b07b0cf
ArfinHasib <arfinhasib21375@gmail.com>
2023-11-28 00:01:11 +0600

    defining update list order

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-container.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-container.tsx
@@ -13,6 +13,14 @@ interface ListContainerProps {
    boardId: string;
 }
 
+function reorder<T>(list: T[], startIndex: number, endIndex: number) {
+   const result = Array.from(list);
+   const [removed] = result.splice(startIndex, 1);
+   result.splice(endIndex, 0, removed);
+
+   return result;
+}
+
 export const ListContainer = ({ data, boardId }: ListContainerProps) => {
    const [orderedData, setOrderedData] = useState(data);
 
@@ -20,8 +28,102 @@ export const ListContainer = ({ data, boardId }: ListContainerProps) => {
       setOrderedData(data);
    }, [data]);
 
+   const onDragEnd = (result: any) => {
+      const { destination, source, type } = result;
+
+      if (!destination) {
+         return;
+      }
+
+      // if  Dopped in the same position
+      if (
+         destination.droppableId === source.droppableId &&
+         destination.index === source.index
+      ) {
+         return;
+      }
+
+      //  User moves a list
+      if (type === 'list') {
+         const items = reorder(
+            orderedData,
+            source.index,
+            destination.index
+         ).map((item, index) => ({ ...item, order: index }));
+
+         setOrderedData(items);
+      }
+
+      // user moves a card
+      if (type === 'card') {
+         let newOrderedData = [...orderedData];
+
+         // Source and destination list
+         const sourceList = newOrderedData.find(
+            (list) => list.id === source.droppableId
+         );
+         const destList = newOrderedData.find(
+            (list) => list.id === destination.droppableId
+         );
+
+         if (!sourceList || !destList) {
+            return;
+         }
+
+         // check if cards exists on the source list
+         if (!sourceList.cards) {
+            sourceList.cards = [];
+         }
+
+         // check if cards exists on the destination list
+         if (!destList.cards) {
+            destList.cards = [];
+         }
+
+         // Moving the card in the same list
+         if (source.droppableId === destination.droppableId) {
+            const reorderedCards = reorder(
+               sourceList.cards,
+               source.index,
+               destination.index
+            );
+
+            reorderedCards.forEach((card, idx) => {
+               card.order = idx;
+            });
+
+            sourceList.cards = reorderedCards;
+
+            setOrderedData(newOrderedData);
+            // trigger server actions
+            // user moves the card to another state
+         } else {
+            // remove card frm the source list
+            const [moveCard] = sourceList.cards.splice(source.index, 1);
+
+            // assign the new listId to the move card
+            moveCard.listId = destination.droppableId;
+
+            // Add card to the destination list
+            destList.cards.splice(destination.index, 0, moveCard);
+
+            sourceList.cards.forEach((card, idx) => {
+               card.order = idx;
+            });
+
+            // Update the order for each card in the destination list
+            destList.cards.forEach((card, idx) => {
+               card.order = idx;
+            });
+
+            setOrderedData(newOrderedData);
+            // todo: trigger server action
+         }
+      }
+   };
+
    return (
-      <DragDropContext onDragEnd={() => {}}>
+      <DragDropContext onDragEnd={onDragEnd}>
          <Droppable droppableId='lists' type='list' direction='horizontal'>
             {(provided) => (
                <ol
@@ -36,7 +138,7 @@ export const ListContainer = ({ data, boardId }: ListContainerProps) => {
                   })}
                   {provided.placeholder}
                   <ListForm />
-                  <div className='flex-shrink-0' />
+                  <div className='flex-shrink-0 w-1' />
                </ol>
             )}
          </Droppable>
