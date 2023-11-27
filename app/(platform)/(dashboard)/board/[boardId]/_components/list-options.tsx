5ca82bb5537af99ed09909750803957e6419c5f2
ArfinHasib <arfinhasib21375@gmail.com>
2023-11-27 16:29:11 +0600

    defining copy list action

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-options.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-options.tsx
@@ -17,6 +17,7 @@ import { PopoverClose } from '@radix-ui/react-popover';
 import { MoreHorizontal, X } from 'lucide-react';
 import { deleteList } from '@/actions/delete-list';
 import { toast } from 'sonner';
+import { ElementRef, useRef } from 'react';
 
 interface ListOptionsProps {
    data: List;
@@ -24,14 +25,24 @@ interface ListOptionsProps {
 }
 
 export const ListOptions = ({ data, onAddCard }: ListOptionsProps) => {
+   const closeRef = useRef<ElementRef<'button'>>(null);
+
    const { execute: executeDelete } = useAction(deleteList, {
       onSuccess: (data) => {
          toast.success(`List"${data.title}" deleted`);
+         closeRef.current?.click();
       },
       onError: (error) => {
          toast.error(error);
       },
    });
+
+   const onDelete = (formData: FormData) => {
+      const id = formData.get('id') as string;
+      const boardId = formData.get('boardId') as string;
+
+      executeDelete({ id, boardId });
+   };
 
    return (
       <Popover>
@@ -44,7 +55,7 @@ export const ListOptions = ({ data, onAddCard }: ListOptionsProps) => {
             <div className='text-sm font-medium text-center text-neutral-600 pb-4'>
                List Actions
             </div>
-            <PopoverClose asChild>
+            <PopoverClose ref={closeRef} asChild>
                <Button
                   className='h-auto w-auto p-2 absolute top-2 right-2 text-neutral-600'
                   variant='ghost'
@@ -70,7 +81,7 @@ export const ListOptions = ({ data, onAddCard }: ListOptionsProps) => {
                </FormSubmit>
             </form>
             <Separator />
-            <form>
+            <form action={onDelete}>
                <input hidden name='id' value={data.id} />
                <input hidden name='boardId' id='boardId' value={data.boardId} />
                <FormSubmit
