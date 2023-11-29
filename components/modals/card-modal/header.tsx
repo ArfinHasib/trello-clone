8bc6416b6bf9c3cc2d135914ab62d2b7a60e5b55
ArfinHasib <arfinhasib21375@gmail.com>
2023-11-29 12:36:10 +0600

    Card modal header section finalize

--- components/modals/card-modal/header.tsx
+++ components/modals/card-modal/header.tsx
@@ -1,12 +1,16 @@
 'use client';
 
-import { FormInput } from '@/components/form/form-input';
-import { Skeleton } from '@/components/ui/skeleton';
-import { CardWithList } from '@/types';
-import { useQueryClient } from '@tanstack/react-query';
+import { toast } from 'sonner';
+import { ElementRef, useRef, useState } from 'react';
 import { Layout } from 'lucide-react';
 import { useParams } from 'next/navigation';
-import { ElementRef, useRef, useState } from 'react';
+import { useQueryClient } from '@tanstack/react-query';
+
+import { CardWithList } from '@/types';
+import { useAction } from '@/hooks/use-action';
+import { updateCard } from '@/actions/update-card';
+import { Skeleton } from '@/components/ui/skeleton';
+import { FormInput } from '@/components/form/form-input';
 
 interface HeaderProps {
    data: CardWithList;
@@ -15,6 +19,24 @@ interface HeaderProps {
 export const Header = ({ data }: HeaderProps) => {
    const queryClient = useQueryClient();
    const params = useParams();
+
+   const { execute } = useAction(updateCard, {
+      onSuccess: (data) => {
+         queryClient.invalidateQueries({
+            queryKey: ['card', data.id],
+         });
+
+         queryClient.invalidateQueries({
+            queryKey: ['card-logs', data.id],
+         });
+
+         toast.success(`Renamed to "${data.title}"`);
+         setTitle(data.title);
+      },
+      onError: (error) => {
+         toast.error(error);
+      },
+   });
 
    const inputRef = useRef<ElementRef<'input'>>(null);
 
@@ -25,7 +47,18 @@ export const Header = ({ data }: HeaderProps) => {
    };
 
    const onSubmit = (formData: FormData) => {
-      console.log(formData.get('title'));
+      const title = formData.get('title') as string;
+      const boardId = params.boardId as string;
+
+      if (title === data.title) {
+         return;
+      }
+
+      execute({
+         title,
+         boardId,
+         id: data.id,
+      });
    };
 
    return (
@@ -38,12 +71,11 @@ export const Header = ({ data }: HeaderProps) => {
                   onBlur={onBlur}
                   id='title'
                   defaultValue={title}
-                  className='font-semibold text-xl px-1 to-neutral-700 bg-transparent border-transparent relative -left-1.5 w-[95%] focus-visible:bg-white focus-visible:border-input mb-0.5 truncate'
+                  className='font-semibold text-xl px-1 text-neutral-700 bg-transparent border-transparent relative -left-1.5 w-[95%] focus-visible:bg-white focus-visible:border-input mb-0.5 truncate'
                />
             </form>
-
             <p className='text-sm text-muted-foreground'>
-               in List <span className='underline'>{data.list.title}</span>
+               in list <span className='underline'>{data.list.title}</span>
             </p>
          </div>
       </div>
